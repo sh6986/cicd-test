c5943fc2f9d649a605eecdd0f78e260c15444746
sh6986 <okok960411@gmail.com>
2023-09-19 02:51:01 +0900

    workflow 수정

--- src/main/kotlin/com/example/cicdtest/CicdTestApplication.kt
+++ src/main/kotlin/com/example/cicdtest/CicdTestApplication.kt
@@ -2,6 +2,8 @@ package com.example.cicdtest
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class CicdTestApplication
@@ -9,4 +11,10 @@ class CicdTestApplication
 fun main(args: Array<String>) {
 
 	runApplication<CicdTestApplication>(*args)
+}
+
+@RestController
+class HealthCheckController {
+	@GetMapping("/ping")
+	fun healthCheck(): String = "pong"
 }
